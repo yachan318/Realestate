dce0d1e5ee72372593ce2af85a316e33365f447e
yachan318 <53626273+yachan318@users.noreply.github.com>
2024-01-21 13:12:56 +1100

    created pages and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
 
 export default function App() {
-  return (
- <h1> Taiwan </h1>
-  )
+  return (<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/logout" element={<Logout />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/profile" element={<Profile />} />
+
+  </Routes>
+  </BrowserRouter>)
 }
