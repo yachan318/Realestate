be70b0930d238b185e702eef45b91b97e07ba4c9
yachan318 <53626273+yachan318@users.noreply.github.com>
2024-01-24 21:21:49 +1100

    Created Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Login from './pages/Login';
 import Logout from './pages/Logout';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 
 export default function App() {
   return (<BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
