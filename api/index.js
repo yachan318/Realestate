df82618399bbb7f2067f91400b0efcd0d14e421d
yachan318 <53626273+yachan318@users.noreply.github.com>
2024-01-31 10:39:49 +1100

    connect to database

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect(process.env.MOGO)
+.then(()=>{
+    console.log('Connect to MongoDB');
+})
+.catch((err)=>{
+    console.log(err);
+});
+const app = express (); 
+
+app.listen(3000, () =>{
+    console.log('Server is running on port 3000');
+}
+);
+
+
+
